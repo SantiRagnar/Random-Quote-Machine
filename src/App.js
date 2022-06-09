31f54422fdedf6db80cefb4b9dfc3089dc4b0f92
SantiRagnar <81393471+SantiRagnar@users.noreply.github.com>
2022-06-09 16:00:11 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ function App() {
   let API_URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
   const [quotes, setquotes] = useState();
   const [author, setauthor] = useState();
+  const [arrQuotes, setarrQuotes] = useState()
 
   const getQuotes = (url)=>{
     fetch(url)
       .then(res => res.json())
       .then(data => {
+        setarrQuotes(data);
         let random = Math.round(Math.random()*data.quotes.length)
         setquotes(data.quotes[random].quote)
         setauthor(data.quotes[random].author)
@@ -20,13 +22,13 @@ function App() {
       .catch(e => console.log(e))
   }
 
-  useEffect(() => {
+  useEffect(()=>{
     getQuotes(API_URL);
   }, [])
 
   return (
     <div className="App">
-        <Quote text={quotes} author={author} RandomQuotes={getQuotes()} url={API_URL}/>
+        <Quote text={quotes} author={author} arr={arrQuotes}/>
     </div>
   );
 }
--- src/componentes/Quote.js
+++ src/componentes/Quote.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../style/Quote.css'
 
-export function Quote({text, author, randomQuotes}){
-
-    function getRamdomQuote(){
-        const button = document.getElementById("new-quote");
+export function Quote({text, author, arr}){
+    const [rquote, setrquote] = useState()
+    const [rauthor, setrauthor] = useState()
+    
+    function rQuotes(){
+        let random = arr.quotes[Math.round(Math.random()*arr.quotes.length)];
+        text = random.quote;
+        author = random.author;
+        setrquote(text);
+        setrauthor(author);
     }
 
     return (
         <div id="quote-box">
             <div className="quote-text">
-                <i className="fa-solid fa-quote-left"></i><span id="text"> {text} </span><i className="fa-solid fa-quote-right"></i>
+                <i className="fa-solid fa-quote-left"></i><span id="text"> {rquote === undefined ? text : rquote} </span><i className="fa-solid fa-quote-right"></i>
             </div>
             <div className="quote-author">
-                <span id="author">-{author}</span>
+                <span id="author">-{rauthor === undefined ? author : rauthor}</span>
             </div>
             <div className="quote-footer">
-            <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="'+ text  + '" ' + author} id="tweet-quote" target="_blank"><i className="fa-brands fa-twitter-square"></i></a>
-            <button id="new-quote" >New Quote</button>
+            <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="'+ (rquote === undefined ? text : rquote) + '" ' + (rauthor === undefined ? author : rauthor)} id="tweet-quote" target="_blank"><i className="fa-brands fa-twitter-square"></i></a>
+            <button id="new-quote" onClick={rQuotes}>New Quote</button>
             </div>
         </div>
     );
